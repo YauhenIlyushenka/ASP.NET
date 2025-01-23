7f1a8e58bb394b2de04231b37d7a07aeb1c56428
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2025-01-23 21:46:16 +0100

    Small fix

--- NoSQL/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.WebHost/Infrastructure/EfDbInitializer.cs
+++ NoSQL/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.WebHost/Infrastructure/EfDbInitializer.cs
@@ -14,7 +14,7 @@ namespace Pcf.ReceivingFromPartner.WebHost.Infrastructure
 			var scope = application.ApplicationServices.CreateScope();
 			var dbContext = scope.ServiceProvider.GetService<T>();
 
-			dbContext.Database.EnsureDeleted();
+			//dbContext.Database.EnsureDeleted();
 			dbContext.Database.Migrate();
 			Seed(scope.ServiceProvider);
 		}
