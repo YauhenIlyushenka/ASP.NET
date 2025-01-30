0d4123cbeab64d42527337e8a481a91bc627a31c
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2025-01-30 21:31:36 +0100

    Homework 7
    - Created shared library for MassTransit;

---
+++ SharedLibraries/Pcf.MassTransitContracts/Pcf.MassTransitContracts/RoutingKeyConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pcf.MassTransitContracts
+{
+	public static class RoutingKeyConstants
+	{
+		public const string PromocodeNotificationKey = "promocode_released";
+	}
+}
