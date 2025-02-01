e8188a9d1e6189a199374f6953dd4ce954f93855
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2025-02-01 01:41:06 +0100

    Homework 7 RabbitMQ / MassTransit
    - Adjusted Dockefiles for 3 microservices: Administration, GivingToCustomer and ReceivingFromPartner;
    - Connected Masstransit for work with RabbitMQ;
    - Replaced sync calls via HttpClients to other microservices on async connections with implementation Producers/Consumers via MassTransit/RabbitMQ;
    - For connections between producers/consumers the shared custom library Pcf.MassTransitContracts has been developed, boxed in Nuget package and pushed to private packages;
    - Adjusted CI yaml files for 3 microservices: Administration, GivingToCustomer and ReceivingFromPartner;

---
+++ NoSQL/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/RabbitMQ/RabbitMQGateway.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Pcf.MassTransitContracts;
+using Pcf.MassTransitContracts.Messages;
+using Pcf.ReceivingFromPartner.Core.Abstractions.Gateways;
+using Pcf.ReceivingFromPartner.Core.Domain;
+using Pcf.ReceivingFromPartner.Core.Helpers;
+using System.Threading.Tasks;
+
+namespace Pcf.ReceivingFromPartner.Integration.RabbitMQ
+{
+	public class RabbitMQGateway : IRabbitMQGateway
+	{
+		private readonly IPublishEndpoint _endpoint;
+		private readonly ILogger<RabbitMQGateway> _logger;
+
+		public RabbitMQGateway(
+			IPublishEndpoint endpoint,
+			ILogger<RabbitMQGateway> logger)
+		{
+			_endpoint = endpoint;
+			_logger = logger;
+		}
+
+		public async Task SendNotificationAboutGivingPromocode(PromoCode promoCode)
+		{
+			var message = new PromocodeNotificationMessage
+			{
+				PartnerId = promoCode.Partner.Id,
+				BeginDate = promoCode.BeginDate.ToDateString(),
+				EndDate = promoCode.EndDate.ToDateString(),
+				PreferenceId = promoCode.PreferenceId,
+				PromoCode = promoCode.Code,
+				ServiceInfo = promoCode.ServiceInfo,
+				PartnerManagerId = promoCode.PartnerManagerId,
+			};
+
+			await _endpoint.Publish<PromocodeNotificationMessage>(message, x =>
+			{
+				x.SetRoutingKey(RoutingKeyConstants.PromocodeNotificationKey);
+			});
+
+			_logger.LogInformation($"Giving promocode: {message.PromoCode} has been sent in the queue successfully");
+		}
+	}
+}
